0e32c3d4ab8154bc8979c08af11e437a0174169d
Haydon Houghton <haydonhoughton@outlook.com>
2020-01-15 13:41:13 +0000

    Creating an interactive menu for program flow

--- directory_copy.rb
+++ directory_copy.rb
@@ -11,33 +11,6 @@
 #  {name: "Joffrey Baratheon", cohort: :november},
 #  {name: "Norman Bates", cohort: :november},
 #]
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-
-    case selection
-    when "1"
-      students = input_students
-
-    when "2"
-      print_header
-      print_names(students)
-      print_count(students)
-
-    when "9"
-      exit
-
-    else
-      puts "I don't know what to do with that input..."
-    end
-  end
-end
-
 
 def input_students
   puts "Please enter the name of the students"
@@ -60,14 +33,15 @@ def print_header
 end
 
 def print_names(names)
-  names.each do |x|
-    puts "#{x[:name]} (#{x[:cohort]} cohort)"
+  names.each.with_index(1) do |x, y|
+    puts "#{y}. #{x[:name]} (#{x[:cohort]} cohort)"
   end
 end
 
 def print_count(names)
   puts "Overall we have #{names.count} great students"
 end
-
-
-interactive_menu
+students = input_students
+print_header
+print_names(students)
+print_count(students)
